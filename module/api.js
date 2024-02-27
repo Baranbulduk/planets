d4705c3c58d40185cf390873ca78fce748c094e7
Baranbulduk <baranbulduk99@gmail.com>
2024-02-27 16:47:59 +0100

    Vecka 2 tis klar

--- module/api.js
+++ module/api.js
@@ -1,1 +1,84 @@
-//export { fetchBaseURL }
+const baseURL =
+    "https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/bodies";
+
+let bodies;
+
+// fetch API
+async function fetchBaseURL(planetName) {
+    try {
+        let response = await fetch(baseURL, {
+            method: "GET",
+            headers: {
+                "x-zocom": "solaris-edVCa1E6zDZRztaq",
+            },
+        });
+        let data = await response.json();
+        bodies = data;
+        bodies = bodies.bodies;
+
+        let planetIndex;
+        if (planetName) {
+            planetIndex = bodies.findIndex(
+                (body) => body.name.toLowerCase() === planetName.toLowerCase()
+            );
+        } else {
+            console.error("Ingen planet specificerad!");
+        }
+
+        displayPlanetInfo(planetIndex);
+    } catch (error) {
+        console.error("Error fetching data:", error);
+        if (error !== null && error !== undefined) {
+            console.log(error.message);
+        } else {
+            console.log("An unknown error occurred.");
+        }
+    }
+}
+
+function displayPlanetInfo(i) {
+    document.getElementById("planet_heading").innerHTML = bodies[i].name;
+    document.getElementById("planet_latin").innerHTML = bodies[i].latinName;
+    document.getElementById("planet_description").innerHTML = bodies[i].desc;
+    document.getElementById("planet_circumference").innerHTML =
+        bodies[i].circumference.toLocaleString() + " km";
+    document.getElementById("planet_distance").innerHTML =
+        bodies[i].distance.toLocaleString() + " km";
+    document.getElementById("planet_temp_day").innerHTML =
+        bodies[i].temp.day + "C";
+    document.getElementById("planet_temp_night").innerHTML =
+        bodies[i].temp.night + "C";
+    if (bodies[i].moons == 0) {
+        document.getElementById("planet_moons").innerHTML = "Har inga månar";
+    } else {
+        document.getElementById("planet_moons").innerHTML =
+            bodies[i].moons.join(", ");
+    }
+}
+
+// Sökruta
+let searchForm = document.getElementById("search-form");
+let searchBox = document.getElementById("search-box");
+document.addEventListener("DOMContentLoaded", function () {
+    searchForm.addEventListener("submit", (event) => {
+        event.preventDefault();
+        searchPlanets();
+    });
+});
+
+function searchPlanets() {
+    let search = searchBox.value.toLowerCase();
+    for (let i = 0; i < bodies.length; i++) {
+        let planetName = bodies[i].name.toLowerCase();
+        if (planetName === search) {
+            window.location.href = `/planet.html?name=${planetName}`;
+        }
+    }
+}
+
+let urlParams = new URLSearchParams(window.location.search);
+let planetName = urlParams.get("name");
+
+fetchBaseURL(planetName);
+
+export { fetchBaseURL, displayPlanetInfo, searchPlanets }
--- script.js
+++ script.js
@@ -1,77 +1,8 @@
-//import { fetchBaseURL } from "./module/api.js";
+import { fetchBaseURL, displayPlanetInfo, searchPlanets } from "./module/api.js";
 
-const baseURL = 'https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/bodies';
-
-let bodies;
-
-// fetch API
-async function fetchBaseURL() {
-    try {
-        let response = await fetch(baseURL, {
-            method: 'GET',
-            headers: {
-                'x-zocom':
-                    'solaris-edVCa1E6zDZRztaq'
-            },
-        });
-        let data = await response.json();
-        bodies = data;
-        bodies = bodies.bodies;
-
-        displayEarthInfo();
-
-    } catch (error) {
-        console.error('Error fetching data:', error);
-        if (error !== null && error !== undefined) {
-            console.log(error.message);
-        } else {
-            console.log("An unknown error occurred.");
-        }
-    }
-}
 fetchBaseURL();
-
-function displayEarthInfo(i) {
-    document.getElementById("earth_heading").innerHTML = bodies[i].name;
-    document.getElementById("earth_latin").innerHTML = bodies[i].latinName;
-    document.getElementById("earth_description").innerHTML = bodies[i].desc;
-    document.getElementById("earth_circumference").innerHTML = bodies[i].circumference.toLocaleString() + " km";
-    document.getElementById("earth_distance").innerHTML = bodies[i].distance.toLocaleString() + " km";
-    document.getElementById("earth_temp_day").innerHTML = bodies[i].temp.day + "C";
-    document.getElementById("earth_temp_night").innerHTML = bodies[i].temp.night + "C";
-    document.getElementById("earth_moons").innerHTML = bodies[i].moons;
-}
-
-// Sökruta 
-let searchForm = document.getElementById("search-form");
-let searchBox = document.getElementById("search-box");
-document.addEventListener("DOMContentLoaded", function () {
-    searchForm.addEventListener("submit", (event) => {
-        console.log(searchBox.value);
-        event.preventDefault();
-        searchPlanets();
-    })
-});
-
-function searchPlanets() {
-    let search = searchBox.value.toLowerCase();
-    for (let i = 0; i < bodies.length; i++) {
-        let planetName = bodies[i].name.toLowerCase();
-        if (planetName === search) {
-            displayEarthInfo(i);
-            //goToPlanetPage(bodies[i].name);
-            return;
-        }
-    }
-}
-
-function goToPlanetPage() {
-    fetchBaseURL();
-    //window.location.href = `/${planetName.toLowerCase()}.html`;
-}
-
-
-
+displayPlanetInfo();
+searchPlanets();
 
 /*
 // Change Pages funkar!!
@@ -80,53 +11,4 @@ document.addEventListener("DOMContentLoaded", function () {
     if (earthElement) {
         earthElement.addEventListener("click", earthPage);
     }
-});
-
-// Earth page
-function earthPage() {
-    fetchBaseURL();
-    window.location.href = "/jorden.html";
-}
-*/
-// Change Pages
-/*document.getElementById("sun").addEventListener("click", sunPage);
-function sunPage() {
-    window.location.href = "";
-}
-document.getElementById("mercury").addEventListener("click", mercuryPage);
-function mercuryPage() {
-    window.location.href = "";
-}
-document.getElementById("venus").addEventListener("click", venusPage);
-function venusPage() {
-    window.location.href = "";
-}
-document.getElementById("earth").addEventListener("click", earthPage);
-function earthPage() {
-    window.location.href = "/earth.html";
-}
-document.getElementById("mars").addEventListener("click", marsPage);
-function marsPage() {
-    window.location.href = "";
-}
-document.getElementById("jupiter").addEventListener("click", jupiterPage);
-function jupiterPage() {
-    window.location.href = "";
-}
-document.getElementById("saturn").addEventListener("click", saturnPage);
-function saturnPage() {
-    window.location.href = "";
-}
-document.getElementById("uranus").addEventListener("click", uranusPage);
-function uranusPage() {
-    window.location.href = "";
-}
-document.getElementById("neptune").addEventListener("click", neptunePage);
-function neptunePage() {
-    window.location.href = "";
-}*/
-
-
-
-
-
+});*/
